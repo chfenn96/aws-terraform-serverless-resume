b04e6e1c3c3ca979f95417d86377f7c9680c0fc6
chfenn96 <ch.fenn@pm.me>
2026-04-03 15:08:24 -0400

    refactor: modularize terraform configuration into standard file structure
    - Created variables.tf to handle AWS region and bucket naming prefixes
    - Created outputs.tf to centralize resource reporting and IDs
    - Replaced hardcoded values in main.tf with variable interpolations
    - Added cloudfront_distribution_id to outputs for future CI/CD invalidations

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "bucket_name_prefix" {
+  description = "The prefix for the S3 bucket"
+  type        = string
+  default     = "my-cloud-resume-bucket"
+}
+
+variable "region" {
+  description = "The AWS region to deploy to"
+  type        = string
+  default     = "us-east-1"
+}
